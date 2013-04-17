23455dfa38a9ac85c0561544d51c6a1662a6be9f
Aki <aki.onetime@gmail.com>
2013-04-18 08:27:28 +0900

    rails new . -m http://railswizard.org/90848b8e40c98184b86c.rb -Te

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RspecEnvSpike::Application.config.secret_token = 'c3e8c4ed9c67b8d5c48099d23ed559c365e8016b0bdd7b0b913d5361ddc2fea379ec278611159f7b13b8e0202ee573b2f0488770e48a7ec0b4643e5c6283898f'
